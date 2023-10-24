e53b6fdcfdfbc50f1fbd3df19db075343b9b2ed5
esa junita pratiwi <145321634+haesaa1006@users.noreply.github.com>
2023-10-24 13:10:47 +0700

    menambahkan routing

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,16 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/anggota', 'AnggotaController::tampil');
+$routes->get('/anggota/form', 'AnggotaController::form');
+$routes->post('/anggota', 'AnggotaController::create');
+
+$routes->post('/anggota/hapus', 'AnggotaController::delete');
+
+$routes->get('/anggota/edit/(:num)', 'AnggotaController::edit/$1');
+$routes->get('/anggota/foto/(:num).png', 'AnggotaController::foto/$1');
+
+$routes->get('/login', 'LoginController::form');
+$routes->post('/login', 'LoginController::masuk');
+$routes->get('/logout', 'LoginController::keluar');
